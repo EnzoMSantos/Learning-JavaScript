aacdb18b1d56be83ff81e09ab552be2e38a301f5
EnzoMSantos <enzosantos2502@gmail.com>
2023-01-26 19:09:42 -0300

    mais 2 excercicios

---
+++ JavaScript/Aula-04/Ex.004.js
@@ -0,0 +1,13 @@
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+
+if( pais == 'brasil') {
+    console.log('Brasileiro')
+}
+else if(pais == 'EUA') {
+    console.log('Americano')
+}
+else {
+    console.log('Estrangeiro')
+}
