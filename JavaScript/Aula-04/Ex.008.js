afdcb79fc6ded77c4f928c5173cec6ff7384d7ef
EnzoMSantos <enzosantos2502@gmail.com>
2023-02-01 15:25:07 -0300

    udpt

--- JavaScript/Aula-04/Ex.008.js
+++ JavaScript/Aula-04/Ex.008.js
@@ -1,10 +1,11 @@
-var hora = 3
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`são exatamente ${hora} horas.`)
 
 if(hora < 12 && hora >= 6){
     console.log('Bom dia!')
 }
-else if(hora <=18) {
+else if(hora <=18 && hora > 12) {
     console.log('Boa tarde!')
 }
 else if(hora <=5) {
