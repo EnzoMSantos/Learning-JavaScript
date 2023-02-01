2a6b2d3cccb78b5aad0289e5fae144e1700483fd
EnzoMSantos <enzosantos2502@gmail.com>
2023-02-01 16:37:09 -0300

    terminando a pag

--- JavaScript/Site 1/Modelo/script.js
+++ JavaScript/Site 1/Modelo/script.js
@@ -3,15 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} horas.`
-
     if (hora >= 0 && hora < 12) {
         img.src = 'Manha COMPLETA.png'
+        document.body.style.background = "#efd493"
     } 
     else if (hora >= 12 && hora <= 18) {
         img.src = 'Tarde COMPLETA.png'
+        document.body.style.background = "#a8b6c6"
     }
     else {
         img.src = 'Noite COMPLETA.png'
+        document.body.style.background = "#071e21"
     }
 }
+
