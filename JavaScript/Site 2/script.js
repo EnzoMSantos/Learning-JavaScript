26aac5945a8cac17ef6a454b201b46ee9cf2398b
EnzoMSantos <enzosantos2502@gmail.com>
2023-02-02 18:43:08 -0300

    editando

--- JavaScript/Site 2/script.js
+++ JavaScript/Site 2/script.js
@@ -1,1 +1,27 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano') //ano de nascimento
+    var res = document.querySelector('div#res') //resultado
+
+    if (fano.value.lenght == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }
+    else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var substanivo = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            substanivo = 'm'
+        }
+        else if (fsex[1].checked) {
+            genero = 'Mulher'
+            substanivo ='ma'
+        
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectei u${substanivo} ${genero} com ${idade} anos.`
+    }
+}
