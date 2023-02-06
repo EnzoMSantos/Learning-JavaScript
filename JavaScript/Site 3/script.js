f4d41420faf2933a8ecf8ed227aa66ad95791ee1
EnzoMSantos <enzosantos2502@gmail.com>
2023-02-06 20:51:11 -0300

    terminando pag

--- JavaScript/Site 3/script.js
+++ JavaScript/Site 3/script.js
@@ -1,22 +1,32 @@
-function contar() {
-var ini = document.getElementById('tinicio')
-var fim = document.getElementById('tfim')
-var passo = document.getElementById('tpasso')
-var res = document.getElementById('res')
+function contar () {
+let ini = document.getElementById('tinicio')
+let fim = document.getElementById('tfim')
+let passo = document.getElementById('tpasso')
+let res = document.getElementById('res')
 
     if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     }
-    else {
-        res.innerHTML = 'Contando'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value) 
-    }
+        else {
+            res.innerHTML = 'Contando'
+            let i = Number(ini.value)
+            let f = Number(fim.value)
+            let p = Number(passo.value) 
+        
 
-    for (let c = 1; c <= f; c += p){
-        res.innerHTML += ` ${c} \u{1F449}`
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } 
+        else {
+            //contagem regressiva
+            for (let c = i; c >= f; c -= p ){
+                res.innerHTML += ` ${c}  \u{1F449} `
+            }
+        }   
+        res.innerHTML += `\u{1F3C1}`
     }
-
-    res.innerHTML += `\u{1F3C1}`
 }
