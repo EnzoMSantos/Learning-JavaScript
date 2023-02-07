fe1591df0f7c3cf6479a0fbf24c4af9cd401acd2
EnzoMSantos <enzosantos2502@gmail.com>
2023-02-07 15:54:32 -0300

    ajustes

--- JavaScript/Aulas/Ex.011.js
+++ JavaScript/Aulas/Ex.011.js
@@ -3,6 +3,10 @@ var valores = [1, 2, 3, num * 3, num.length - 1, 4 , 5, 7, 8, 12, 62]
 valores.sort()
 num.push(2)
 num.sort()
+let poss = valores.indexOf(5) + num.indexOf(2)
+var posi = Number(poss)
+console.log(posi)
+
 
 for(var pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
