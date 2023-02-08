05ab09bbe7b1db97d325005b3563429bba7d4373
EnzoMSantos <enzosantos2502@gmail.com>
2023-02-08 17:00:35 -0300

    terminando site

--- JavaScript/Site 5/script.js
+++ JavaScript/Site 5/script.js
@@ -1,11 +1,71 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
+let valores = []
+
+function isNumber(n) {
+    if(Number(n) >= 1 &&  Number(n) <= 100) {
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 function AddNumero() {
+    if(isNumber(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} Adcionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }
+    else {
+        window.alert('Valor inválido ou já encontrado na lista!')
+    }
 
+    num.value = ''
+    num.focus()
 }
 
 function Finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adcione valores antes de finalizar!')
+    }
+    else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores) {
+            soma += valores[pos]
+            if(valores[pos] > maior) {     
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor) {
+                menor = valores[pos]
+    
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todos temos ${total} números cadastrados </p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior} </p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor} </p>`
+        res.innerHTML += `<p> A soma dos valores é ${soma} </p>`
+        res.innerHTML += `<p> A média dos valores é ${media} </p>`
+    }
+}
+
 
 }
